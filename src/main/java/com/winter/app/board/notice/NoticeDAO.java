d3b63dc5906874c04fcf5a42681c29efb9066f6f
hjin <hjin9473@gmail.com>
2025-11-25 17:44:20 +0900

    11/25

---
+++ src/main/java/com/winter/app/board/notice/NoticeDAO.java
@@ -0,0 +1,21 @@
+package com.winter.app.board.notice;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NoticeDAO {
+	
+	public NoticeDTO detail(NoticeDTO noticeDTO) throws Exception;
+	
+	public List<NoticeDTO> list() throws Exception;
+	
+	public int add(NoticeDTO noticeDTO) throws Exception;
+	
+	public int update(NoticeDTO noticeDTO) throws Exception;
+	
+	public int delete(NoticeDTO noticeDTO) throws Exception;
+	
+	
+}
